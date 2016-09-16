d6df5db040a7f398e30659b29e83e1608395783f
Yuliia Vovk <smyulia96@gmail.com>
2016-09-16 13:25:44 +0300

    Updated rdbms-image, fixed issues with riak-image

--- riak-image/devicehive-start.sh
+++ riak-image/devicehive-start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+nginx &
+
 set -x
 
 while ! curl --output /dev/null --silent --head --fail "http://${DH_RIAK_HOST}:${DH_RIAK_HTTP_PORT}/"; do
@@ -7,7 +9,7 @@ while ! curl --output /dev/null --silent --head --fail "http://${DH_RIAK_HOST}:$
   sleep 5
 done
 
-sleep 30 # delay for bucket-type bootstrapping
+sleep 60 # delay for bucket-type bootstrapping
 
 curl -XPUT \
     -H "Content-Type: application/json" \
@@ -73,8 +75,8 @@ curl -XPOST \
 
 curl -XPUT \
     -H "Content-Type: application/json" \
-    -H 'x-riak-index-guid_bin: E50D6085-2ABA-48E9-B1C3-73C673E414BE' \
-    -d "{\"id\":1, \"guid\":\"E50D6085-2ABA-48E9-B1C3-73C673E414BE\", \"name\":\"Sample VirtualLed Device\", \"status\":\"Offline\", \"network\": {\"id\":1,\"key\":null,\"name\":\"VirtualLed Sample Network\", \"description\":\"A DeviceHive network for VirtualLed sample\",\"entityVersion\":null}, \"deviceClass\":{\"id\":1, \"name\":\"Sample VirtualLed Device\", \"permanent\": false, \"offlineTimeout\": 600, \"data\":null,\"equipment\":null}, \"blocked\":null}" \
+    -H 'x-riak-index-guid_bin: e50d6085-2aba-48e9-b1c3-73c673e414be' \
+    -d "{\"id\":1, \"guid\":\"e50d6085-2aba-48e9-b1c3-73c673e414be\", \"name\":\"Sample VirtualLed Device\", \"status\":\"Offline\", \"network\": {\"id\":1,\"key\":null,\"name\":\"VirtualLed Sample Network\", \"description\":\"A DeviceHive network for VirtualLed sample\",\"entityVersion\":null}, \"deviceClass\":{\"id\":1, \"name\":\"Sample VirtualLed Device\", \"permanent\": false, \"offlineTimeout\": 600, \"data\":null,\"equipment\":null}, \"blocked\":null}" \
     http://${DH_RIAK_HOST}:${DH_RIAK_HTTP_PORT}/types/default/buckets/device/keys/1
 
 curl -XPOST \
@@ -84,17 +86,17 @@ curl -XPOST \
 
 curl -XPUT \
     -H "Content-Type: application/json" \
-    -d "{\"value\":\"true\"}" \
+    -d "{\"value\":\"false\"}" \
     http://${DH_RIAK_HOST}:${DH_RIAK_HTTP_PORT}/types/default/buckets/configuration/keys/google.identity.allowed
 
 curl -XPUT \
     -H "Content-Type: application/json" \
-    -d "{\"value\":\"google_id\"" \
+    -d "{\"value\":\"google_id\"}" \
     http://${DH_RIAK_HOST}:${DH_RIAK_HTTP_PORT}/types/default/buckets/configuration/keys/google.identity.client.id
 
 curl -XPUT \
     -H "Content-Type: application/json" \
-    -d "{\"value\":\"true\"}" \
+    -d "{\"value\":\"false\"}" \
     http://${DH_RIAK_HOST}:${DH_RIAK_HTTP_PORT}/types/default/buckets/configuration/keys/facebook.identity.allowed
 
 curl -XPUT \
@@ -104,7 +106,7 @@ curl -XPUT \
 
 curl -XPUT \
     -H "Content-Type: application/json" \
-    -d "{\"value\":\"true\"}" \
+    -d "{\"value\":\"false\"}" \
     http://${DH_RIAK_HOST}:${DH_RIAK_HTTP_PORT}/types/default/buckets/configuration/keys/github.identity.allowed
 
 curl -XPUT \
@@ -143,7 +145,7 @@ java -server -Xmx512m -XX:MaxRAMFraction=1 -XX:+UseConcMarkSweepGC -XX:+CMSParal
 -Dflyway.enabled=false \
 -Driak.host=${DH_RIAK_HOST} \
 -Driak.port=${DH_RIAK_PORT} \
--Dmetadata.broker.list=${DH_KAFKA_ADDRESS}:${DH_KAFKA_PORT} \
+-Dbootstrap.servers=${DH_KAFKA_ADDRESS}:${DH_KAFKA_PORT} \
 -Dzookeeper.connect=${DH_ZK_ADDRESS}:${DH_ZK_PORT} \
 -Dthreads.count=${DH_KAFKA_THREADS_COUNT:-3} \
 -Dhazelcast.port=${DH_HAZELCAST_PORT:-5701} \
